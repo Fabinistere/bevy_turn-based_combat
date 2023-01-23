2655716a4357d2898a8258e3c22955a0ef2198a0
Wabtey <samurai.banzai@laposte.net>
2023-01-23 19:15:54 +0100

    add block skill

--- src/combat/skill_list.rs
+++ src/combat/skill_list.rs
@@ -5,13 +5,28 @@ use crate::combat::skills::{Skill, SkillType, TargetSide};
 impl Skill {
     pub fn bam() -> Self {
         Skill {
+            skill_type: SkillType::Attack,
             target_side: TargetSide::Enemy,
+            target_number: 1,
             // invalidate by the target side but just in case.
             self_cast: false,
-            skill_type: SkillType::Attack,
             hp_dealt: 150,
             initiative: 50,
             description: String::from("Bam"),
+            ..Default::default()
+        }
+    }
+
+    pub fn block() -> Self {
+        Skill {
+            skill_type: SkillType::Defense,
+            target_side: TargetSide::Ally,
+            target_number: 1,
+            // invalidate by the target side but just in case.
+            self_cast: true,
+            shield_dealt: 200,
+            initiative: 50,
+            description: String::from("Block"),
             ..Default::default()
         }
     }
